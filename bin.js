6af3feb3134533e326afe44d7911e15a10189fe6
Oli Evans <oli@protocol.ai>
2022-08-31 14:48:03 +0100

    feat: cli `print`, `index`, & `report` commands (#2)
    Adds cli commands to print the linkdex, write it to file, or report on dag completeness.
    
    Pass it one or more CARs. Here is a quick demo of what the `linkdex` CLI can do:
    
    ```bash
    # Print indexes to stdout
    $ linkdex print thing-1.car thing-2.car
    bafybeiaalbzxtx6jxr5znrtjtxvdn76s7zxl7e4rw6mvrbjz6gahhnc54y --> bafybeietimcnchtujzibl4vqyvrcosagvfandacatiees4zed6updxdssi
    bafybeietimcnchtujzibl4vqyvrcosagvfandacatiees4zed6updxdssi --> bafkreic6ukg4v5hi7yt2qcebjpm2hkh34sejiiatr46wtiyntiwvlrskae
    bafybeietimcnchtujzibl4vqyvrcosagvfandacatiees4zed6updxdssi --> bafkreiabfvaraoupn66wrdcgy3l2bb7vwfqha2y4u3owtqw5hc75uhneyq
    
    # Writes indexes to file per CAR input
    $ linkdex index thing-1.car thing-2.car
    $ ls
    thing-1.car thing-1.car.linkdex thing-2.car thing-2.car.linkdex
    
    # Report the dag structure from the union of all the CARs
    $ linkdex report thing-1.car thing-2.car
    {"structure":"Complete","blockCount":21}
    
    # Exit with error if any block links to a CID that is not in the set of blocks.
    $ linkdex report thing-1.car --error-if-partial
    {"structure":"Partial","blockCount":10}
    Error: CAR(s) contain partial DAG
    $ echo $? # 1
    ```
    
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- bin.js
+++ bin.js
@@ -1,9 +1,96 @@
 #!/usr/bin/env node
 
 import fs from 'fs'
-import { linkdex } from "./index.js"
+import sade from 'sade'
+import { linkdex, decodedBlocks } from './index.js'
+import { pipeline } from 'node:stream/promises'
 
-const input = process.argv[2]
-const inStream = fs.createReadStream(input)
+const pkg = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url)))
 
-linkdex(inStream)
+const cli = sade('linkdex')
+
+cli
+  .version(pkg.version)
+  .example('report partial.car')
+
+cli.command('report <car>')
+  .describe('Print a linkdex report for a car')
+  .option('--error-if-partial')
+  .action(async (first, opts) => {
+    const cars = [first, ...opts._]
+    const index = new Map()
+    for (const car of cars) {
+      await pipeline(
+        fs.createReadStream(car),
+        async function * (source) {
+          for await (const {src, dest} of linkdex(source)){
+            let linkSet = index.get(src)
+            if (!linkSet) {
+              linkSet = new Set()
+              index.set(src, linkSet)
+            }
+            if (dest) {
+              linkSet.add(dest)
+            }
+          }
+        }
+      )
+    }
+    const res = report(index)
+    console.log(JSON.stringify(res))
+    if (opts['error-if-partial'] && res.structure === 'Partial') {
+      console.error('Error: CAR(s) contain partial DAG')
+      process.exit(1)
+    }
+  })
+
+function report (index) {
+  for (const [src, links] of index.entries()) {
+    for (const link of links) {
+      if (!index.has(link)) {
+        return { structure: 'Partial', blockCount: index.size }
+      }
+    }
+  }
+  return { structure: 'Complete', blockCount: index.size }
+}
+
+cli.command('print <car>')
+  .describe('Print index for a car')
+  .option('--mermaid')
+  .action(async (first, opts) => {
+    const cars = [first, ...opts._]
+    if(opts.mermaid) {
+      console.log('```mermaid')
+      console.log('graph LR')
+    }
+    for (const car of cars) {
+      const inStream = fs.createReadStream(car)
+      for await (const {src, dest} of linkdex(inStream)){
+        console.log(dest ? `${src} --> ${dest}` : src)
+      }
+    }
+    if(opts.mermaid) {
+      console.log('```')
+    }
+  })
+
+cli.command('index <car>')
+  .describe('Write out an index for a car to <car>.linkdex')
+  .action(async (first, opts) => {
+    const cars = [first, ...opts._]
+    for (const car of cars) {
+      const file = `${car}.linkdex`
+      await pipeline(
+        fs.createReadStream(car),
+        async function* (source) {
+          for await (const {src, dest} of linkdex(source)){
+            yield `${src} --> ${dest}\n`
+          }
+        },
+        fs.createWriteStream(file)
+      )
+    }
+  })
+
+cli.parse(process.argv)
